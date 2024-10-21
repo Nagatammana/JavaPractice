63d051071d7c5b66d3453ed46215aae19e2c60b4
Nagatammana <145829992+Nagatammana@users.noreply.github.com>
2024-10-21 12:08:09 +0530

    Update Branch.java

--- Branch.java
+++ Branch.java
@@ -2,7 +2,8 @@ class Branch{
     public static void main(String[] args){
         int a=3;
         int b=4;
-        int c=a+b;
+        int e=5;
+        int c=a+b+e;
         System.out.println(c);
     }
 }
